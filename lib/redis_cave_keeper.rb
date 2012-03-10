8c0c6db188fe9b70f7ae0fb3fd230c33027dab5c
Nils Haldenwang <n.haldenwang@googlemail.com>
2012-03-10 12:06:44 +0100

    Adds #unlock.

--- lib/redis_cave_keeper.rb
+++ lib/redis_cave_keeper.rb
@@ -1,23 +1,40 @@
 require "redis_cave_keeper/version"
 
 class RedisCaveKeeper
-  attr_reader :lock_time, :redis, :lock_key, :timeout
+  attr_reader :lock_time, :redis, :lock_key, :timeout, :key
 
   class CaveKeeperError < StandardError; end
-  class LockFailure < CaveKeeperError; end
+  class LockError < CaveKeeperError; end
+  class UnlockError < CaveKeeperError; end
 
   def initialize(redis, key, opts = {})
     @redis    = redis
+    @key      = key
     @lock_key = "cave-keeper-lock:#{key}"
     @timeout  = opts[:timeout] || 5
   end
 
   def lock
-    raise LockFailure, "Key is already locked." if has_lock? 
+    raise LockError, "Key '#{key}' is already locked." if has_lock? 
     unless try_to_acquire_lock
       acquire_lock_if_expired
     end
     has_lock?
+  end
+
+  def unlock
+    raise UnlockError, "Key '#{key}' is not locked." unless has_lock?
+    raise UnlockError, "The lock for the key '#{key}' is expired." if lock_expired?
+    if redis.getset(lock_key, ( now + timeout + 1 )).to_i < now 
+      # Someone else acquired the lock via getset.
+      raise UnlockError, "The lock for the key '#{key}' is expired."
+    else
+      # Our lock is extended by timeout + 1, so we safely can
+      # perform a delete.
+      redis.del(lock_key)
+      reset
+      true
+    end
   end
 
   def has_lock?
@@ -26,16 +43,15 @@ class RedisCaveKeeper
 
   protected
   def acquire_lock_if_expired
-    if other_lock_expired?
+    if lock_expired?
       # Use getset here to make sure no one else
       # acquired a lock in the meantime.
       acquire_lock if now > secure_set_expiration
     end
-    reset unless has_lock?
-    has_lock?
+    reset_unless_locked
   end
 
-  def other_lock_expired?
+  def lock_expired?
     now > redis.get( lock_key ).to_i
   end
 
@@ -47,26 +63,27 @@ class RedisCaveKeeper
     if redis.setnx( lock_key, expiration_timestamp )
       acquire_lock
     end
+    reset_unless_locked
+  end
+
+  def reset_unless_locked
     reset unless has_lock?
     has_lock?
   end
 
   def acquire_lock
     @locked = true
-    @locked_until = expiration_timestamp
   end
 
   def expiration_timestamp
-    @expiration_timestamp ||= ( Time.now.to_i + timeout + 1 ) 
+    Time.now.to_i + timeout + 1 
   end
 
   def now
-    @now ||= Time.now.to_i
+    Time.now.to_i
   end
 
   def reset
-    @expiration_timestamp = nil
-    @locked_until = nil
     @now = nil
     @locked = false
   end
--- spec/redis_cave_keeper_spec.rb
+++ spec/redis_cave_keeper_spec.rb
@@ -7,61 +7,98 @@ describe RedisCaveKeeper do
   let(:keeper_without_lock) { RedisCaveKeeper.new(redis, key) } 
   let(:keeper_with_lock)    { keeper_without_lock.tap(&:lock) } 
 
-  context "when it has the lock" do
-    subject { keeper_with_lock }
+  context "#lock" do
+    context "when it has the lock" do
+      subject { keeper_with_lock }
 
-    it "should raise an error if one tries to reacquire the lock" do
-      expect do
+      it "should raise an error if one tries to reacquire the lock" do
+        expect do
+          subject.lock
+        end.to raise_error(RedisCaveKeeper::LockError)
+      end
+    end
+
+    context "when the key is not locked before" do
+      subject { keeper_without_lock }
+
+      it "should be able to acquire the lock if the key is not locked" do
+        subject.lock.should be_true
+      end
+
+      it "should be locked after successfully acquiring a lock" do
         subject.lock
-      end.to raise_error(RedisCaveKeeper::LockFailure)
+        subject.should have_lock
+      end
+    end
+
+    context "when the lock key is set but expired" do
+      subject { keeper_without_lock }
+
+      before(:each) do
+        redis.set lock_key, (Time.now.to_i - 42) 
+      end
+
+      it "should be able to get the lock, if the timestamp is expired" do
+        subject.lock.should be_true
+      end
+
+      it "should set an expiration time in the future" do
+        subject.lock
+        redis.get(lock_key).to_i.should > Time.now.to_i
+      end
+    end
+
+    context "when the key is locked with a valid timestamp" do
+      subject { keeper_without_lock }
+
+      before(:each) do
+        redis.set lock_key, ( Time.now.to_i + 10 )
+      end
+
+      it "should not have a lock if the locking failed" do
+        subject.lock
+        subject.should_not have_lock
+      end
+
+      it "should return false on #lock if it fails" do
+        subject.lock.should be_false
+      end
     end
   end
 
-  context "when the key is not locked before" do
-    subject { keeper_without_lock }
+  context "#unlock" do
+    context "when locked and not expired" do
+      subject { keeper_with_lock }
 
-    it "should be able to acquire the lock if the key is not locked" do
-      subject.lock.should be_true
+      it "unlocks successfully" do
+        subject.unlock.should be_true      
+        subject.should_not have_lock
+      end  
     end
 
-    it "should be locked after successfully acquiring a lock" do
-      subject.lock
-      subject.should have_lock
-    end
-  end
+    context "when not locked" do
+      subject { keeper_without_lock }
 
-  context "when the lock key is set but expired" do
-    subject { keeper_without_lock }
-
-    before(:each) do
-      redis.set lock_key, (Time.now.to_i - 42) 
+      it "should raise an error if trying to unlock without lock" do
+        expect do
+          subject.unlock 
+        end.to raise_error(RedisCaveKeeper::UnlockError)
+      end
     end
 
-    it "should be able to get the lock, if the timestamp is expired" do
-      subject.lock.should be_true
-    end
+    context "when the lock is expired" do
+      subject { keeper_with_lock }
 
-    it "should set an expiration time in the future" do
-      subject.lock
-      redis.get(lock_key).to_i.should > Time.now.to_i
-    end
-  end
-  
-  context "when the key is locked with a valid timestamp" do
-    subject { keeper_without_lock }
+      before(:each) do
+        subject
+        redis.set lock_key, (Time.now.to_i - 42) 
+      end
 
-    before(:each) do
-      redis.set lock_key, ( Time.now.to_i + 10 )
-    end
-
-    it "should not have a lock if the locking failed" do
-      subject.lock
-      subject.should_not have_lock
-    end
-
-    it "should return false on #lock if it fails" do
-      subject.lock.should be_false
+      it "should raise an error" do
+        expect  do
+          subject.unlock
+        end.to raise_error(RedisCaveKeeper::UnlockError)
+      end
     end
   end
-
 end
